f742620053b0a4c9f55746b32a2e4b472fed4122
Mehboob-alam1 <ma8122576@gmail.com>
2022-11-10 18:36:28 +0000

    Login Validations done in this part

--- app/src/main/java/com/mehboob/widgetsabout/MainActivity.java
+++ app/src/main/java/com/mehboob/widgetsabout/MainActivity.java
@@ -4,58 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
-    Button btnSubmit;
-    ToggleButton toggleButton1, toggleButton2;
-    Switch aSwitch;
+import com.mehboob.widgetsabout.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
+private static final String TAG="MainActivity";
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        toggleButton1 = findViewById(R.id.toggle1);
-        toggleButton2 = findViewById(R.id.toggle2);
-        btnSubmit = findViewById(R.id.btnSubmit);
-        aSwitch = findViewById(R.id.switch1);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.btnLogin.setOnClickListener(view -> {
+
+            if (binding.etEmail.getText().toString().isEmpty()){
+                binding.etEmail.setError("Email required");
+            }else if (binding.etPassword.getText().toString().isEmpty()){
+                binding.etPassword.setError("Password required");
+            }else if (binding.etEmail.getText().toString().equals("mehboobcodes@gmail.com") && binding.etPassword.getText().toString().equals("123456")){
+                Toast.makeText(this, "Successfully login", Toast.LENGTH_SHORT).show();
+            }else  if (!binding.etEmail.getText().toString().equals("mehboobcodes@gmail.com")){
+                Log.d(TAG,"Wrong email");
+                binding.etEmail.setError("Wrong email");
+            }else  if (!binding.etPassword.getText().toString().equals("123456")){
+                Log.d(TAG,"Wrong password");
+                binding.etPassword.setError("Wrong password");
+            }
 
 
-        btnSubmit.setOnClickListener(view -> {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("Toggle1 State ").append(toggleButton1.getText());
-            stringBuilder.append("\nToggle2 State ").append(toggleButton2.getText());
-            stringBuilder.append("\n\nSwitch ").append(aSwitch.isChecked());
-
-            Toast.makeText(this, "" + stringBuilder, Toast.LENGTH_SHORT).show();
 
 
         });
 
 
+
+
     }
 
 
-    public void customToast() {
 
 
-        LayoutInflater li = getLayoutInflater();
-
-
-        View view = li.inflate(R.layout.custom_toast, null);
-
-        Toast toast = new Toast(MainActivity.this);
-        toast.setView(view);
-        toast.setDuration(Toast.LENGTH_LONG);
-        toast.show();
-
-    }
 }
